833ad11df3e5b5dc6199230a0c4c7c00135a32b3
Revuj <revuj112@gmail.com>
2021-03-21 14:29:21 +0000

    updated global stylesheet

--- components/navbar.js
+++ components/navbar.js
@@ -1,36 +1,37 @@
 import React from 'react';
 import Link from 'next/link';
+import style from './navbar.module.css';
 
 const navbar = () => {
   return (
-    <navbar id="navbar">
+    <navbar id={style.navbar}>
       <ul>
-        <li className="nav-item">
+        <li className={style.navItem}>
           <Link href="/" as={process.env.BACKEND_URL + '/'}>
             <a>Home</a>
           </Link>
         </li>
-        <li className="nav-item">
+        <li className={style.navItem}>
           <Link href="/solutions" as={process.env.BACKEND_URL + '/solutions'}>
             <a>Solutions</a>
           </Link>
         </li>
-        <li className="nav-item">
+        <li className={style.navItem}>
           <Link href="/team">
             <a>Team</a>
           </Link>
         </li>
-        <li className="nav-item">
+        <li className={style.navItem}>
           <Link href="/contact">
             <a>Contact</a>
           </Link>
         </li>
-        <li className="nav-item">
+        <li className={style.navItem}>
           <Link href="/press">
             <a>Press</a>
           </Link>
         </li>
-        <li className="nav-item">
+        <li className={style.navItem}>
           <Link href="/blog">
             <a>Blog</a>
           </Link>
