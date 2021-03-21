54612182543bb746cdba22fd520f964ffab67e65
Revuj <revuj112@gmail.com>
2021-03-21 22:47:51 +0000

    'meet the team section' finished

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
+import style from '../styles/home.module.css';
+import { FiArrowRightCircle } from 'react-icons/fi';
 
 export default function Home() {
   return (
@@ -13,7 +16,31 @@ export default function Home() {
         </Head>
         <Navbar dark={true} page={'home'} />
         <main>
-          <h1 className="title">Nice Content</h1>
+          <section id={style.meetTheTeam}>
+            <span id={style.content}>
+              <span id={style.textContent}>
+                <h2>Meet the team</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+                  culpa modi corporis. Hic sequi possimus dolorum commodi
+                  dolorem blanditiis, accusantium atque nulla, quibusdam
+                  reiciendis voluptate maxime corrupti, delectus eos odit.
+                </p>
+                <Link href="/team" as={process.env.BACKEND_URL + '/team'}>
+                  <a id={style.meetTheTeamButton}>
+                    <FiArrowRightCircle size={30} />
+                    <span id={style.buttonText}>Meet our team members</span>
+                  </a>
+                </Link>
+              </span>
+              <img
+                id={style.meetTheTeamImg}
+                src="/assets/meet-the-team.png"
+                width={956}
+                height={637}
+              />
+            </span>
+          </section>
         </main>
         <Footer dark={false} />
       </div>
