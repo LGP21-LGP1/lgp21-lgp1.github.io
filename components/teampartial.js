0b94a28c509d70af91a0c553ce53db42cd8200b3
J. Luz <up201703782@fe.up.pt>
2021-03-21 23:07:06 +0000

    Added leaders section

--- components/teampartial.js
+++ components/teampartial.js
@@ -11,22 +11,25 @@ const TeamPartial = (props) => {
     <div className={styles.partial_div}>
       <div
         className={styles.partial_header}
-        style={{ justifyContent: props.align }}
+        style={{ textAlign: props.align, justifyContent: props.align }}
       >
         <div className={styles.partial_header_tex}>
           <h1 className="semibold-text">{teamName}</h1>
           <span className="regular-text">{teamDescription}</span>
         </div>
       </div>
-      <div className={styles.partial_members_div}>
+      {/* <div className={styles.partial_members_div}> */}
+      <div className="mt-5 d-flex row w-100 mx-auto">
         {teamMembers.map((member, index) => {
           return (
-            <TeamMember
-              key= {member.photo + index}
-              photo={member.photo}
-              name={member.name}
-              role={member.role}
-            />
+            <div className="col-6 col-md-3 col-lg-2">
+              <TeamMember
+                key={member.photo + index}
+                photo={member.photo}
+                name={member.name}
+                role={member.role}
+              />
+            </div>
           );
         })}
       </div>
--- pages/team.js
+++ pages/team.js
@@ -1,11 +1,35 @@
 import Head from 'next/head';
 import { useState } from 'react';
 import Navbar from '../components/navbar';
+import Footer from '../components/footer';
 import PageTitle from '../components/pagetitle';
 import TeamPartial from '../components/teampartial';
 import styles from '../styles/team.module.css';
+import TeamMember from '../components/TeamMember';
 
 export default function Team() {
+  const [leaders, setLeaders] = useState([
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'João Luz',
+      role: 'DEVELOPER',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'Maria Adelaide',
+      role: 'SENHORA DOUTORA',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'João Luz',
+      role: 'DEVELOPER',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'Maria Adelaide',
+      role: 'SENHORA DOUTORA',
+    },
+  ]);
   const [teams, setTeams] = useState([
     {
       name: 'AirMedDigital',
@@ -62,53 +86,6 @@ export default function Team() {
           name: 'Maria Adelaide',
           role: 'SENHORA DOUTORA',
         },
-      ],
-    },
-    {
-      name: 'AirMedDigital',
-      description:
-        ' AirMedDigital Description Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempus nisl accumsan velit tincidunt, eget condimentum lectus venenatis. Nullam volutpat venenatis nulla, ut porta mauris. Donec eget efficitur tortor.',
-      members: [
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
         {
           photo: 'https://www.w3schools.com/howto/img_avatar.png',
           name: 'João Luz',
@@ -126,6 +103,83 @@ export default function Team() {
       description:
         ' AirMedDigital Description Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempus nisl accumsan velit tincidunt, eget condimentum lectus venenatis. Nullam volutpat venenatis nulla, ut porta mauris. Donec eget efficitur tortor.',
       members: [
+        {
+          photo: 'https://www.w3schools.com/howto/img_avatar.png',
+          name: 'João Luz',
+          role: 'DEVELOPER',
+        },
+        {
+          photo: 'https://www.w3schools.com/w3images/avatar6.png',
+          name: 'Maria Adelaide',
+          role: 'SENHORA DOUTORA',
+        },
+        {
+          photo: 'https://www.w3schools.com/howto/img_avatar.png',
+          name: 'João Luz',
+          role: 'DEVELOPER',
+        },
+        {
+          photo: 'https://www.w3schools.com/w3images/avatar6.png',
+          name: 'Maria Adelaide',
+          role: 'SENHORA DOUTORA',
+        },
+        {
+          photo: 'https://www.w3schools.com/howto/img_avatar.png',
+          name: 'João Luz',
+          role: 'DEVELOPER',
+        },
+        {
+          photo: 'https://www.w3schools.com/w3images/avatar6.png',
+          name: 'Maria Adelaide',
+          role: 'SENHORA DOUTORA',
+        },
+        {
+          photo: 'https://www.w3schools.com/howto/img_avatar.png',
+          name: 'João Luz',
+          role: 'DEVELOPER',
+        },
+        {
+          photo: 'https://www.w3schools.com/w3images/avatar6.png',
+          name: 'Maria Adelaide',
+          role: 'SENHORA DOUTORA',
+        },
+        {
+          photo: 'https://www.w3schools.com/howto/img_avatar.png',
+          name: 'João Luz',
+          role: 'DEVELOPER',
+        },
+        {
+          photo: 'https://www.w3schools.com/w3images/avatar6.png',
+          name: 'Maria Adelaide',
+          role: 'SENHORA DOUTORA',
+        },
+        {
+          photo: 'https://www.w3schools.com/howto/img_avatar.png',
+          name: 'João Luz',
+          role: 'DEVELOPER',
+        },
+        {
+          photo: 'https://www.w3schools.com/w3images/avatar6.png',
+          name: 'Maria Adelaide',
+          role: 'SENHORA DOUTORA',
+        },
+      ],
+    },
+    {
+      name: 'AirMedDigital',
+      description:
+        ' AirMedDigital Description Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempus nisl accumsan velit tincidunt, eget condimentum lectus venenatis. Nullam volutpat venenatis nulla, ut porta mauris. Donec eget efficitur tortor.',
+      members: [
+        {
+          photo: 'https://www.w3schools.com/howto/img_avatar.png',
+          name: 'João Luz',
+          role: 'DEVELOPER',
+        },
+        {
+          photo: 'https://www.w3schools.com/w3images/avatar6.png',
+          name: 'Maria Adelaide',
+          role: 'SENHORA DOUTORA',
+        },
         {
           photo: 'https://www.w3schools.com/howto/img_avatar.png',
           name: 'João Luz',
@@ -179,9 +233,9 @@ export default function Team() {
       ],
     },
   ]);
-
+  const nteams = teams.length;
   return (
-    <div className="container">
+    <div className="page-container">
       <Head>
         <title>Vortex - Team</title>
         <link rel="icon" href="/favicon.ico" />
@@ -193,27 +247,45 @@ export default function Team() {
           nisl accumsan velit tincidunt, eget condimentum lectus venenatis.
           Nullam volutpat"
       />
+      <div className="my-5 w-100 d-flex flex-column justify-content-center align-items-center">
+        <h3 className="bold-text mb-4">Our Leaders</h3>
+        <div className="w-75 d-flex flex-wrap row align-items-center">
+          {leaders.map((member, index) => {
+            return (
+              <div className="col-md-6 col-lg-3">
+                <TeamMember
+                  key={member.photo + index}
+                  photo={member.photo}
+                  name={member.name}
+                  role={member.role}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
       {teams.map((team, index) => {
         const align = index % 2 == 0 ? 'start' : 'end';
         return (
-          <>
-            <div style={{ textAlign: align }}>
+          <div key={team.name + index}>
+            <div>
               <TeamPartial
-                key={team + index}
                 name={team.name}
                 description={team.description}
                 members={team.members}
                 align={align}
               />
             </div>
-            <div className={styles.delimiter}>
-              <hr
-                className={` ${styles.hr} ${
-                  index % 2 == 0 ? styles.hr_end : styles.hr_start
-                }`}
-              />
-            </div>
-          </>
+            {index != nteams - 1 && (
+              <div className={`mb-5 ${styles.delimiter}`}>
+                <hr
+                  className={`${styles.hr} ${
+                    index % 2 == 0 ? styles.hr_end : styles.hr_start
+                  }`}
+                />
+              </div>
+            )}
+          </div>
         );
       })}
       <Footer dark={true} />
