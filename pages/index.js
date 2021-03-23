ba1a3146c9cbcac8841db42d942268ead156fe88
Revuj <revuj112@gmail.com>
2021-03-23 12:07:09 +0000

    solutions section responsive

--- pages/index.js
+++ pages/index.js
@@ -30,8 +30,7 @@ export default function Home() {
                 <p>
                   Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
                   diam nonumy eirmod tempor invidunt ut labore et dolore magna
-                  aliquyam erat, sed diam voluptua. At vero eos et accusam et
-                  justo duo dolores et ea rebum.
+                  aliquyam erat, sed diam voluptua.
                 </p>
               </div>
             </div>
