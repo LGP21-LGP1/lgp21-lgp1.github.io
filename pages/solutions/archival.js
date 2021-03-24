0fe44fa5396bebcde123495049ded0bbaa0f0dd2
zmartz <jdcmtm@gmail.com>
2021-03-24 00:40:04 +0000

    Solutions pages

--- pages/solutions/archival.js
+++ pages/solutions/archival.js
@@ -1,18 +1,39 @@
 import Head from 'next/head';
 import Navbar from '../../components/navbar';
 import Footer from '../../components/footer';
+import style from '../../styles/solutions.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+
 
 const archival = () => {
   return (
     <>
       <div className="page-container">
         <Head>
-          <title>Vortex - Solution 1</title>
+          <title>Vortex - Solutions: Archival</title>
           <link rel="icon" href="/favicon.ico" />
+          <link rel="stylesheet" href="https://use.typekit.net/ajk2viw.css" />
         </Head>
         <Navbar dark={false} page={'solutions'} />
         <main>
-          <h1 className="title">Solution 1</h1>
+          <h1 className={style.solutiontitle}>Solutions: Archival</h1>
+          <img className={style.solutionimage}
+            src="/assets/logo1.png"
+            alt="Archival solution"
+            width={300}
+            height={300}
+          />
+          <h3 className={style.productname}>Product Name</h3>
+          <p className={style.solutiontext}>Short Project Description</p>
+          <p>
+          <Link href="/solutions/archival-detail">
+            <a className={style.solutionlink}>
+              Read More...
+            </a>
+          </Link>
+          </p>
+
         </main>
         <Footer dark={true} />
       </div>
--- pages/solutions/labs.js
+++ pages/solutions/labs.js
@@ -1,18 +1,39 @@
 import Head from 'next/head';
 import Navbar from '../../components/navbar';
 import Footer from '../../components/footer';
+import style from '../../styles/solutions.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+
 
 const labs = () => {
   return (
     <>
       <div className="page-container">
         <Head>
-          <title>Vortex - Solution 3</title>
+          <title>Vortex - Solutions: Labs</title>
           <link rel="icon" href="/favicon.ico" />
+          <link rel="stylesheet" href="https://use.typekit.net/ajk2viw.css" />
         </Head>
         <Navbar dark={false} page={'solutions'} />
         <main>
-          <h1 className="title">Solution 3</h1>
+          <h1 className={style.solutiontitle}>Solutions: Labs</h1> 
+          <img className={style.solutionimage}
+            src="/assets/logo3.png"
+            alt="Labs solution"
+            width={300}
+            height={300}
+          />
+          <p className={style.productname}>Product Name</p>
+          <p className={style.solutiontext}>Short Project Description</p>
+          <p>
+          <Link href="/solutions/labs-detail">
+            <a className={style.solutionlink}>
+              Read More...
+            </a>
+          </Link>
+          </p>
+
         </main>
         <Footer dark={true} />
       </div>
