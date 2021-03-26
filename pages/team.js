6512e20ae8dbaec8793e99e15548e2340bbe3c56
J. Luz <up201703782@fe.up.pt>
2021-03-26 16:02:53 +0000

    Updated Team Page layout

--- pages/team.js
+++ pages/team.js
@@ -11,226 +11,245 @@ export default function Team() {
   const [leaders, setLeaders] = useState([
     {
       photo: 'https://www.w3schools.com/howto/img_avatar.png',
-      name: 'João Luz',
-      role: 'DEVELOPER',
+      name: 'César Nogueira',
+      role: 'CEO',
     },
     {
       photo: 'https://www.w3schools.com/w3images/avatar6.png',
-      name: 'Maria Adelaide',
-      role: 'SENHORA DOUTORA',
+      name: 'José Silva ',
+      role: 'CTO',
     },
     {
       photo: 'https://www.w3schools.com/howto/img_avatar.png',
-      name: 'João Luz',
-      role: 'DEVELOPER',
+      name: 'Marta Faria',
+      role: 'CBO',
     },
     {
       photo: 'https://www.w3schools.com/w3images/avatar6.png',
-      name: 'Maria Adelaide',
-      role: 'SENHORA DOUTORA',
+      name: 'Mariana Ribeira',
+      role: 'CMO',
     },
   ]);
   const [teams, setTeams] = useState([
     {
-      name: 'AirMedDigital',
-      description:
-        ' AirMedDigital Description Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempus nisl accumsan velit tincidunt, eget condimentum lectus venenatis. Nullam volutpat venenatis nulla, ut porta mauris. Donec eget efficitur tortor.',
-      members: [
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-      ],
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'André Mori',
+      role: 'MIEIC',
     },
     {
-      name: 'AirMedDigital',
-      description:
-        ' AirMedDigital Description Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempus nisl accumsan velit tincidunt, eget condimentum lectus venenatis. Nullam volutpat venenatis nulla, ut porta mauris. Donec eget efficitur tortor.',
-      members: [
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-      ],
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'Augusto Osório',
+      role: 'MM',
     },
     {
-      name: 'AirMedDigital',
-      description:
-        ' AirMedDigital Description Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempus nisl accumsan velit tincidunt, eget condimentum lectus venenatis. Nullam volutpat venenatis nulla, ut porta mauris. Donec eget efficitur tortor.',
-      members: [
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-        {
-          photo: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'João Luz',
-          role: 'DEVELOPER',
-        },
-        {
-          photo: 'https://www.w3schools.com/w3images/avatar6.png',
-          name: 'Maria Adelaide',
-          role: 'SENHORA DOUTORA',
-        },
-      ],
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'Cláudia Martins',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'João Lopes',
+      role: 'MESG',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'João Pacheco',
+      role: 'MM',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'Lucas Stein',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'Luís Oliveira',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'Miguel Romariz',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'Paulo Pinto',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'Sérgio Dias',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'Vitor Ventuzelos',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'Matilde Santiago',
+      role: 'MESG',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'Filipe Barbosa',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'Bernardo Branco',
+      role: 'MM',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'Bruno Vale Fernandes',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'José Miguel Simões',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'José Diogo Martins',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'Margarida Alves Pinho',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'Afonso Carvalho Pereira de Sá',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'Paulo Jorge Palhau Moutinho',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'Leonardo Fernandes Moura',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'Célio Ighour de Castro Rodrigues',
+      role: 'MM',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'Mafalda Santos',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'António Pedro Reis Ribeiro Sousa Dantas',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'Eduardo João Santana Macedo',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'João Pedro Pinto Mota',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'João Rafael Gomes Varela',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'José Pedro Moreira de Almeida Baptista',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'Luís Miguel Pedrosa de Moura Oliveira Henriques',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'Tito Alexandre Trindade Griné',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'Tomás Moreira Santos',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'Jéssica Leão',
+      role: 'MESG',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'Carmen Silva',
+      role: 'MM',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'Gracielle Sampaio',
+      role: 'MM',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'Eduardo Carreira Ribeiro',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'Carlos Jorge Direito Albuquerque',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'Daniel Silva',
+      role: 'MM',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'William Tostes Lobo',
+      role: 'MESG',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'Pedro Esteves',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'Miguel Pires',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'Luís Castro e Costa',
+      role: 'MM',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'André Filipe da Silva Moutinho',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/w3images/avatar6.png',
+      name: 'João Luz',
+      role: 'MIEIC',
+    },
+    {
+      photo: 'https://www.w3schools.com/howto/img_avatar.png',
+      name: 'João Veiga de Macedo',
+      role: 'MIEIC',
     },
   ]);
   const nteams = teams.length;
@@ -245,7 +264,7 @@ export default function Team() {
         title="The Team"
         description="We are a multi-complementary team made up of: IT developers, UI/UX designers and business analysts. We work together to achieve successful solutions. "
       />
-      <div className="my-5 w-100 d-flex flex-column justify-content-center align-items-center">
+      <div className="mt-5 mb-2 w-75 mx-auto d-flex flex-column justify-content-center align-items-center">
         <h3 className="bold-text mb-4">Our Leaders</h3>
         <div className="w-75 d-flex flex-wrap row align-items-center">
           {leaders.map((member, index) => {
@@ -261,28 +280,20 @@ export default function Team() {
           })}
         </div>
       </div>
-      {teams.map((team, index) => {
-        const align = index % 2 == 0 ? 'start' : 'end';
-        return (
-          <div key={team.name + index}>
-            <div className={`mb-5 ${styles.delimiter}`}>
-              <hr
-                className={`${styles.hr} ${
-                  index % 2 == 0 ? styles.hr_start : styles.hr_end
-                }`}
+      <hr className={`w-75 mx-auto ${styles.hr}`} />
+      <div className="mt-5 d-flex w-90 mx-auto d-flex flex-wrap justify-content-center">
+        {teams.map((member, index) => {
+          return (
+            <div className="col-6 col-md-3 col-lg-2" key={member.photo + index}>
+              <TeamMember
+                photo={member.photo}
+                name={member.name}
+                role={member.role}
               />
             </div>
-            <div>
-              <TeamPartial
-                name={team.name}
-                description={team.description}
-                members={team.members}
-                align={align}
-              />
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
       <Footer dark={true} />
     </div>
   );
