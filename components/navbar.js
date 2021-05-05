33333244bb07ad81dc56c29c714247bf70af661a
Revuj <revuj112@gmail.com>
2021-05-05 11:53:35 +0100

    updated navbar

--- components/navbar.js
+++ components/navbar.js
@@ -46,55 +46,17 @@ const navbar = ({ dark, page }) => {
                 <a>Home</a>
               </Link>
             </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle
-                nav
-                className={style.navItem}
-                id={page === 'solutions' ? style.selectedPage : ''}
+            <NavItem
+              className={style.navItem}
+              id={page === 'solutions' ? style.selectedPage : ''}
+            >
+              <Link
+                href="solutions"
+                as={process.env.BACKEND_URL + '/solutions'}
               >
-                Solutions
-              </DropdownToggle>
-              <DropdownMenu id={style.dropdownMenu} right>
-                <DropdownItem className={style.dropdownItem}>
-                  <Link
-                    href="/solutions/photography"
-                    as={process.env.BACKEND_URL + '/solutions/photography'}
-                  >
-                    <a>Photography</a>
-                  </Link>
-                </DropdownItem>
-                <DropdownItem divider className={style.dropdownDivider} />
-                <DropdownItem className={style.dropdownItem}>
-                  {' '}
-                  <Link
-                    href="/solutions/education"
-                    as={process.env.BACKEND_URL + '/solutions/education'}
-                  >
-                    <a>Education</a>
-                  </Link>
-                </DropdownItem>
-                <DropdownItem divider className={style.dropdownDivider} />
-                <DropdownItem className={style.dropdownItem}>
-                  {' '}
-                  <Link
-                    href="/solutions/aviation"
-                    as={process.env.BACKEND_URL + '/solutions/aviation'}
-                  >
-                    <a>Aviation</a>
-                  </Link>
-                </DropdownItem>
-                <DropdownItem divider className={style.dropdownDivider} />
-                <DropdownItem className={style.dropdownItem}>
-                  {' '}
-                  <Link
-                    href="/solutions/research"
-                    as={process.env.BACKEND_URL + '/solutions/research'}
-                  >
-                    <a>Research</a>
-                  </Link>
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
+                <a>Solutions</a>
+              </Link>
+            </NavItem>
             <NavItem
               className={style.navItem}
               id={page === 'team' ? style.selectedPage : ''}
