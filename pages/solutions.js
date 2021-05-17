4281769caf44fe900bcb48a7f840e0920d398807
zmartz <jdcmtm@gmail.com>
2021-05-17 22:42:09 +0100

    Fix sizes

--- pages/solutions.js
+++ pages/solutions.js
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import Footer from '../components/footer';
 import Navbar from '../components/navbar';
 import style from '../styles/solutions_revamp.module.css';
+import OrangeShape from '../public/assets/orange-shape.svg';
 
 export default function Solutions() {
   return (
@@ -21,15 +22,16 @@ export default function Solutions() {
                 alt="Archival solution"
             
             />
+           
             <div className={`${style.solutionDescription} ${style.boxRight}`}>
                 <h2 className={style.solutionTitle}>APP</h2>
-                <p >APP is an information system designed to assist cultural institutions on art pieces' conservation, allowing them to manage all the necessary procedures in an automatic way.	</p>
+                <p className={style.solutionText}>APP is an information system designed to assist cultural institutions on art pieces' conservation, allowing them to manage all the necessary procedures in an automatic way.	</p>
             </div>  
           </div>
           <div className={style.solution} id="evaluate">
             <div className={`${style.solutionDescription} ${style.boxLeft}`}>
                 <h2 className={style.solutionTitle}>e-Valuate</h2>
-                <p>e-Valuate is a platform adapted to remote classes, helping schools to manage aspects related to the classes, assessment cycle and transparency among students and teachers.</p>
+                <p className={style.solutionText}>e-Valuate is a platform adapted to remote classes, helping schools to manage aspects related to the classes, assessment cycle and transparency among students and teachers.</p>
             </div>
             <img className={style.imageSolution}
                 src="/assets/education.png"
@@ -43,13 +45,13 @@ export default function Solutions() {
             />
             <div className={`${style.solutionDescription} ${style.boxRight}`}>
                 <h2 className={style.solutionTitle}>AIR Med Digital</h2>
-                <p>AIR Med Digital is a platform that centralizes medical information in the aviation field, aiming to be more sustainable, straightforward, and reduce bureaucracy on the current processes.</p>
+                <p className={style.solutionText}>AIR Med Digital is a platform that centralizes medical information in the aviation field, aiming to be more sustainable, straightforward, and reduce bureaucracy on the current processes.</p>
             </div>
           </div>
           <div className={style.solution} id="labcentric">
             <div className={`${style.solutionDescription} ${style.boxLeft}`}>
                 <h2 className={style.solutionTitle}>Labcentric</h2>
-                <p>Labcentric is an information system focused on digitizing and monitoring laboratory activities, allowing a decrease in errors, resulting in a cost reduction.</p>
+                <p className={style.solutionText}>Labcentric is an information system focused on digitizing and monitoring laboratory activities, allowing a decrease in errors, resulting in a cost reduction.</p>
             </div>
             <img className={style.imageSolution}
                 src="/assets/research.png"
