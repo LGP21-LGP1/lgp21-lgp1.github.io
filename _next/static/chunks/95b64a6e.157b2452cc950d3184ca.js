eb0fc82bf8aa8902fc95c408e2822fa35373487e
Luywer <Luywer@users.noreply.github.com>
2021-06-01 18:05:39 +0000

    deploy: b12ff6dcce33c9e5547809365a297bfbc871ce6d

---
+++ _next/static/chunks/95b64a6e.157b2452cc950d3184ca.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[6],{IdFE:function(t,n,a){"use strict";a.d(n,"a",(function(){return i}));var c=a("Lnxd");function i(t){return Object(c.a)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M416 199.5h-91.4V64H187.4v135.5H96l160 158.1 160-158.1zM96 402.8V448h320v-45.2H96z"}}]})(t)}}}]);
